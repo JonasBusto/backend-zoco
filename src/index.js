85ac1a83b402b2e7803b6c0124a046056e873b5b
JonasBusto <jonasrex00@gmail.com>
2024-07-12 16:43:34 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import 'dotenv/config';
 import { taskRouter } from './routes/tasks.js';
 import { TaskModel } from './models/task.js';
@@ -7,6 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/tasks', taskRouter({ taskModel: TaskModel }));
 
